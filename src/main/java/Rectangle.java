81eb4088fa7093738086b5305a5944c1c0f3a931
DakMcMak <esbolov.galymzhan@mail.ru>
2017-04-19 23:49:48 +0600

    Initial commit

---
+++ src/main/java/Rectangle.java
@@ -0,0 +1,29 @@
+public class Rectangle {
+    int top, left, width, height;
+
+    public Rectangle(int top, int left, int width, int height) {
+        this.top = top;
+        this.left = left;
+        this.width = width;
+        this.height = height;
+    }
+
+    public Rectangle(int top, int left) {
+        this.top = top;
+        this.left = left;
+        this.width = 0;
+        this.height = 0;
+    }
+
+    public Rectangle(int top, int left, int width) {
+        this.top = top;
+        this.left = left;
+    }
+
+    public Rectangle(Rectangle rec) {
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
